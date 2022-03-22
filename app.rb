4820f12be6e1f69633e510c3e6000689ac0fd899
morris-brasier-nti-johanneberg <morris.brasier@elev.ga.ntig.se>
2022-03-22 14:17:41 +0100

    nav

--- app.rb
+++ app.rb
@@ -5,5 +5,21 @@ require 'bcrypt'
 require 'sinatra/reloader'
 
 get('/') do
-    return slim(:start)
+    slim(:start)
+end
+
+get('/workouts/new') do
+    slim(:workouts/new)
+end
+
+get('/workouts') do
+    slim(:workouts/index) 
+end
+
+get('/info') do
+    slim(:info)
+end
+
+get('/log_in') do
+    slim(:log_in)
 end
