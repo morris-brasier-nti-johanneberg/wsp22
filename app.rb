3b703187d83d5083f6d18129c078ddc88aa011fd
morris-brasier-nti-johanneberg <morris.brasier@elev.ga.ntig.se>
2022-04-22 13:48:02 +0200

    Login sida

--- app.rb
+++ app.rb
@@ -15,24 +15,26 @@ get('/workouts/new') do
 end
 
 get('/workouts') do
+    id = session[:id].to_i
     db = SQLite3::Database.new("db/träning.db")
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM workouts") 
-    p result
-    slim(:"workouts/index", locals:{workouts: result})
-end
-
-get('/workouts/:id') do
-    slim(:"workouts/show")
+    result = db.execute("SELECT * FROM workouts WHERE user_id = ?",id) 
+    p "Alla workouts från result #{result}"
+    slim(:"workouts/index", locals:{workouts:result})
 end
 
 get('/info') do
     slim(:info)
 end
 
-get('/log_in') do
-    slim(:log_in)
-end
+# get('/your_workouts') do
+#     id = session[:id].to_i
+#     db = SQLite3::Database.new('db/träning.db')
+#     db.results_as_hash = true
+#     result = db.execute("SELECT * FROM workouts WHERE user_id = ?",id)
+#     p "Alla workouts från result #{result}"
+#     slim(:"workouts/index",locals:{workouts:result})
+# end
 
 get('/register') do
     slim(:register)
@@ -43,12 +45,33 @@ post('/users/new') do
     password = params[:password]
     password_confirm = params[:password_confirm]
 
-    if password == password_confirm
+    if (password == password_confirm)
         password_digest = BCrypt::Password.create(password)
         db = SQLite3::Database.new('db/träning.db')
-        db.execute("INSERT INTO users (username,password) VALUES (?,?)", username, password)
+        db.execute("INSERT INTO users (username, password) VALUES (?,?)", username, password_digest)
         redirect('/register')
     else
         "Passwords does not match!"
+    end
+end
+
+get('/show_login') do
+    slim(:login)
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/träning.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["password"]
+    id = result["id"]
+
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        redirect('/workouts') 
+    else
+        "Wrong password!"
     end
 end
