9ded88308fe5b41fd88d1861d5044cae3a2882c4
morris-brasier-nti-johanneberg <morris.brasier@elev.ga.ntig.se>
2022-04-24 22:21:52 +0200

    crud

--- app.rb
+++ app.rb
@@ -19,6 +19,52 @@ get('/workouts') do
     slim(:"workouts/index", locals:{workouts:result})
 end
 
+get('/workouts/new') do
+    slim(:"workouts/new")
+end
+
+post('/workouts/new') do
+    id = session[:id].to_i
+    workout_name = params[:workout_name]
+    user_id = params[:user_id].to_i
+    db = SQLite3::Database.new("db/träning.db")
+    db.execute("INSERT INTO workouts (name, user_id) VALUES (?,?)",workout_name,user_id)
+    redirect('/workouts')
+end
+
+# post('/exercise/new') do
+#     id = session[:id].to_i
+#     exercise_name = params[:exercise_name]
+#     sets = params[:sets].to_i
+#     reps = params[:reps].to_i
+#     db = SQLite3::Database.new("db/träning.db")
+#     db.execute("INSERT INTO exercises (name, sets, reps) VALUES (?,?,?) ",exercise_name,sets,reps)
+#     redirect("/workouts")
+# end
+
+post("/workouts/:id/delete") do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/träning.db")
+    db.execute("DELETE FROM workouts WHERE id = ?",id)
+    redirect('/workouts')
+end
+
+post("/workouts/:id/update") do
+    id = params[:id].to_i
+    workout_name = params[:workout_name]
+    db = SQLite3::Database.new("db/träning.db")
+    db.execute("UPDATE workouts SET name = ? WHERE id = ?",workout_name,id)
+    redirect('/workouts')
+end
+
+get("/workouts/:id/edit") do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/träning.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM workouts WHERE id = ?",id).first
+    slim(:"workouts/edit",locals:{result:result})
+end
+
 get("/workouts/:id") do
     id = params[:id].to_i
     db = SQLite3::Database.new("db/träning.db")
@@ -28,10 +74,6 @@ get("/workouts/:id") do
     result2 = db.execute("SELECT * FROM exercises WHERE id = (SELECT exercise_id FROM workout_exercise_rel WHERE workout_id = (SELECT id FROM workouts WHERE id = ?))",id)
     p "resultat2 är #{result2}"
     slim(:"workouts/show", locals:{result:result,result2:result2})
-end
-
-get('/workouts/new') do
-    slim(:"workouts/new")
 end
 
 get('/info') do
