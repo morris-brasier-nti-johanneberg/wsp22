ef522718a497d2d296921f6cca1ba6a871cf8f08
morris-brasier-nti-johanneberg <morris.brasier@elev.ga.ntig.se>
2022-04-24 16:08:41 +0200

    CRUD (Read)

--- app.rb
+++ app.rb
@@ -10,31 +10,33 @@ get('/') do
     slim(:start)
 end
 
-get('/workouts/new') do
-    slim(:"workouts/new")
-end
-
 get('/workouts') do
     id = session[:id].to_i
     db = SQLite3::Database.new("db/träning.db")
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM workouts WHERE user_id = ?",id) 
+    result = db.execute("SELECT * FROM workouts WHERE user_id = ?",id)
     p "Alla workouts från result #{result}"
     slim(:"workouts/index", locals:{workouts:result})
+end
+
+get("/workouts/:id") do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/träning.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM workouts WHERE id = ?",id).first
+    p "result1 är #{result}"
+    result2 = db.execute("SELECT * FROM exercises WHERE id = (SELECT exercise_id FROM workout_exercise_rel WHERE workout_id = (SELECT id FROM workouts WHERE id = ?))",id)
+    p "resultat2 är #{result2}"
+    slim(:"workouts/show", locals:{result:result,result2:result2})
+end
+
+get('/workouts/new') do
+    slim(:"workouts/new")
 end
 
 get('/info') do
     slim(:info)
 end
-
-# get('/your_workouts') do
-#     id = session[:id].to_i
-#     db = SQLite3::Database.new('db/träning.db')
-#     db.results_as_hash = true
-#     result = db.execute("SELECT * FROM workouts WHERE user_id = ?",id)
-#     p "Alla workouts från result #{result}"
-#     slim(:"workouts/index",locals:{workouts:result})
-# end
 
 get('/register') do
     slim(:register)
